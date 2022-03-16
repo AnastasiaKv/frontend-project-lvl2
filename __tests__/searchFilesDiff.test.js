3aa31d6fcef0f0f9bd89fe6494e67be1e0ba8d0c
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-03-16 21:01:56 +0300

    fix: rename folder

--- __tests__/searchFilesDiff.test.js
+++ __tests__/searchFilesDiff.test.js
@@ -14,7 +14,7 @@ const diff = {
 const jsonDiff = _.replace(JSON.stringify(diff, null, 2), /"|,/g, '');
 
 test('genDiff', () => {
-  const path1 = path.join('__fixture__', 'file1.json');
-  const path2 = path.join('__fixture__', 'file2.json');
+  const path1 = path.join('__fixtures__', 'file1.json');
+  const path2 = path.join('__fixtures__', 'file2.json');
   expect(genDiff(path1, path2)).toEqual(jsonDiff);
 });
