dace883b151a4239ebc6497ba5b5ae3ca7908d7e
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-03-17 23:38:16 +0300

    test: add and refactor some tests

--- __tests__/searchFilesDiff.test.js
+++ __tests__/searchFilesDiff.test.js
@@ -1,20 +1,28 @@
 import _ from 'lodash';
-import path from 'path';
+import * as path from 'path';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/searchFilesDiff.js';
 
-/* eslint-disable */
-const diff = {
-  "- follow": false,
-  "  host": "hexlet.io",
-  "- proxy": "123.234.53.22",
-  "- timeout": 50,
-  "+ timeout": 20,
-  "+ verbose": true
-};
-const jsonDiff = _.replace(JSON.stringify(diff, null, 2), /"|,/g, '');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-test('genDiff', () => {
-  const path1 = path.join('__fixtures__', 'file1.json');
-  const path2 = path.join('__fixtures__', 'file2.json');
-  expect(genDiff(path1, path2)).toEqual(jsonDiff);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getExpectedData = (filename) => {
+  const rawdata = readFile(filename);
+  const obj = JSON.parse(rawdata);
+  return _.replace(JSON.stringify(obj, null, 2), /"|,/g, '');
+}
+
+test('generate data diff', () => {
+  const expected1 = getExpectedData('expected1.json');
+  const path1 = path.join('..', '__fixtures__', 'file1.json');
+  const path2 = path.join('..', '__fixtures__', 'file2.json');
+  expect(genDiff(path1, path2)).toEqual(expected1);
+
+
+  const expected2 = getExpectedData('expected2.json');
+  const falsePath = path.join('..', '__fixtures__', 'file3.json');
+  expect(genDiff(path1, falsePath)).toEqual(expected2);
 });
