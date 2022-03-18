fd0afec7d50a9c58739e797a22f525b6d7372f82
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-03-18 16:19:00 +0300

    test: edit tests/ add some new

--- __tests__/searchFilesDiff.test.js
+++ __tests__/searchFilesDiff.test.js
@@ -7,22 +7,35 @@ import genDiff from '../src/searchFilesDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getRelativePath = (filename) => path.join('__fixtures__', filename);
+const getAbsolutePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getAbsolutePath(filename), 'utf-8');
 const getExpectedData = (filename) => {
   const rawdata = readFile(filename);
   const obj = JSON.parse(rawdata);
   return _.replace(JSON.stringify(obj, null, 2), /"|,/g, '');
 };
 
+const jsonPath1 = 'file1.json';
+const jsonPath2 = 'file2.json';
+const ymlPath1 = 'file1.yml';
+const ymlPath2 = 'file2.yml';
+
 /* eslint-disable */
 test('generate data diff', () => {
-  const expected1 = getExpectedData('expected1.json');
-  const path1 = path.join('..', '__fixtures__', 'file1.json');
-  const path2 = path.join('..', '__fixtures__', 'file2.json');
-  expect(genDiff(path1, path2)).toEqual(expected1);
+  const expected = getExpectedData('expected.json');
 
-  const expected2 = getExpectedData('expected2.json');
-  const falsePath = path.join('..', '__fixtures__', 'file3.json');
-  expect(genDiff(path1, falsePath)).toEqual(expected2);
+  expect(genDiff(getRelativePath(jsonPath1), getRelativePath(jsonPath2))).toEqual(expected);
+  expect(genDiff(getAbsolutePath(jsonPath1), getAbsolutePath(jsonPath2))).toEqual(expected);
+  expect(genDiff(getRelativePath(ymlPath1), getRelativePath(ymlPath2))).toEqual(expected);
+});
+
+test('file doesn\'t exist', () => {
+  expect(() => {
+    genDiff(getRelativePath(jsonPath1), getRelativePath('notExist.json'))
+  }).toThrow();
+
+  expect(() => {
+    genDiff(getRelativePath('notExist1.json'), getRelativePath('notExist2.json'))
+  }).toThrow();
 });
