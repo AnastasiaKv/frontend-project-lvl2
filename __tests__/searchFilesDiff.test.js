504bfe1d62daf46799af385993312134c4f3b19e
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-03-24 19:57:59 +0300

    tests: update fixtures and tests

--- __tests__/searchFilesDiff.test.js
+++ __tests__/searchFilesDiff.test.js
@@ -1,41 +1,59 @@
-import _ from 'lodash';
-import * as path from 'path';
 import * as fs from 'fs';
+import * as path from 'path';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/searchFilesDiff.js';
+import genDiff from '../index.js';
+import { stylish } from '../src/formaters.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getRelativePath = (filename) => path.join('__fixtures__', filename);
 const getAbsolutePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getAbsolutePath(filename), 'utf-8');
-const getExpectedData = (filename) => {
-  const rawdata = readFile(filename);
-  const obj = JSON.parse(rawdata);
-  return _.replace(JSON.stringify(obj, null, 2), /"|,/g, '');
-};
 
 const jsonPath1 = 'file1.json';
 const jsonPath2 = 'file2.json';
 const ymlPath1 = 'file1.yml';
 const ymlPath2 = 'file2.yml';
 
-/* eslint-disable */
-test('generate data diff', () => {
-  const expected = getExpectedData('expected.json');
+let expected = '';
 
-  expect(genDiff(getRelativePath(jsonPath1), getRelativePath(jsonPath2))).toEqual(expected);
-  expect(genDiff(getAbsolutePath(jsonPath1), getAbsolutePath(jsonPath2))).toEqual(expected);
-  expect(genDiff(getRelativePath(ymlPath1), getRelativePath(ymlPath2))).toEqual(expected);
+beforeAll(() => {
+  const rawdata = fs.readFileSync(getAbsolutePath('expected.json'), 'utf-8');
+  expected = stylish(JSON.parse(rawdata));
+});
+
+test('json format / relative pathes', () => {
+  const actual = genDiff(
+    getRelativePath(jsonPath1),
+    getRelativePath(jsonPath2),
+  );
+  expect(actual).toEqual(expected);
+});
+
+test('json format / absolute pathes', () => {
+  const actual = genDiff(
+    getAbsolutePath(jsonPath1),
+    getAbsolutePath(jsonPath2),
+  );
+  expect(actual).toEqual(expected);
+});
+
+test('yml format', () => {
+  const actual = genDiff(
+    getRelativePath(ymlPath1),
+    getRelativePath(ymlPath2),
+  );
+  expect(actual).toEqual(expected);
 });
 
 test('file doesn\'t exist', () => {
-  expect(() => {
-    genDiff(getRelativePath(jsonPath1), getRelativePath('notExist.json'))
-  }).toThrow();
+  expect(() => genDiff(
+    getRelativePath(jsonPath1),
+    'notExist.json',
+  )).toThrow();
 
-  expect(() => {
-    genDiff(getRelativePath('notExist1.json'), getRelativePath('notExist2.json'))
-  }).toThrow();
+  expect(() => genDiff(
+    'notExist1.json',
+    'notExist2.json',
+  )).toThrow();
 });
