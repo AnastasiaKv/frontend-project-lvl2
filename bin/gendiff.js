4638008197c32d36b7376c8daa08a51e715c2c26
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-03-28 17:08:50 +0300

    fix: update  utility

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,7 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/searchFilesDiff.js';
-import { plain } from '../src/formaters.js';
 
-const formater = (fp1, fp2, format) => {
-  switch (format) {
-    case 'plain': return genDiff(fp1, fp2, plain);
-    default: return genDiff(fp1, fp2);
-  }
-};
 const program = new Command();
 
 program
@@ -19,7 +12,8 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    console.log(formater(filepath1, filepath2, options.format));
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
   });
 
 program.parse();
