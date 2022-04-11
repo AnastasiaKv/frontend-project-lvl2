57990a98b5e5e21cbdb45cd7da35eeac08b29bc3
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-04-11 16:35:24 +0300

    refactor: simplify gendiff func

--- src/searchFilesDiff.js
+++ src/searchFilesDiff.js
@@ -1,29 +1,24 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import process from 'process';
+import { readFileSync } from 'fs';
+import path from 'path';
 import parser from './parsers.js';
 import formatter from './formatters/index.js';
-import genFlatDiffTree from './genFlatDiffTree.js';
+import genDiffTree from './genDiffTree.js';
 
-const normalizePath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(normalizePath(filepath), 'utf-8');
+const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
 const getFileExtname = (filepath) => path.extname(filepath).replace('.', '');
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const normalizedFilepath1 = normalizePath(filepath1);
-  const normalizedFilepath2 = normalizePath(filepath2);
+  const extname1 = getFileExtname(filepath1);
+  const extname2 = getFileExtname(filepath2);
 
-  const extname1 = getFileExtname(normalizedFilepath1);
-  const extname2 = getFileExtname(normalizedFilepath2);
-
-  const fileData1 = readFile(normalizedFilepath1);
-  const fileData2 = readFile(normalizedFilepath2);
+  const fileData1 = readFile(filepath1);
+  const fileData2 = readFile(filepath2);
 
   const parsedFileData1 = parser(fileData1, extname1);
   const parsedFileData2 = parser(fileData2, extname2);
 
-  const flatDiff = genFlatDiffTree(parsedFileData1, parsedFileData2);
-  return formatter(formatName)(flatDiff);
+  const diffTree = genDiffTree(parsedFileData1, parsedFileData2);
+  return formatter(formatName)(diffTree);
 };
 
 export default genDiff;
