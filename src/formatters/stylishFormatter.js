66bc699c6a881701e161a3a22c4f4a592149ac15
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-04-14 21:17:23 +0300

    refactor: update stringify and stylish func

--- src/formatters/stylishFormatter.js
+++ src/formatters/stylishFormatter.js
@@ -1,45 +1,39 @@
 import _ from 'lodash';
 
-const buildDiffObject = (diff) => diff.reduce((tree, node) => {
-  switch (node.status) {
-    case 'added':
-      return { ...tree, [`+ ${node.key}`]: node.value };
-    case 'removed':
-      return { ...tree, [`- ${node.key}`]: node.value };
-    case 'nested': {
-      return { ...tree, [node.key]: buildDiffObject(node.children) };
+const getIndent = (depth, isBracketIndent = false, spaceCount = 2) => ' '.repeat(spaceCount * depth + spaceCount * (depth - 1) - (isBracketIndent ? 2 : 0));
+
+const buildObjectStr = (lines, depth) => ['{', ...lines, `${getIndent(depth, true)}}`].join('\n');
+
+const stringify = (currValue, depth) => {
+  if (!_.isPlainObject(currValue)) return currValue;
+  const lines = Object
+    .entries(currValue)
+    .map(([key, value]) => `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
+  return buildObjectStr(lines, depth);
+};
+
+export default (data) => {
+  const iter = (tree, depth = 1) => tree.map((node) => {
+    const indent = getIndent(depth);
+    const valueDepth = depth + 1;
+    const { key, value } = node;
+    const buildEntrieStr = (entrieVal, marker = ' ') => `${indent}${marker} ${key}: ${entrieVal}`;
+    switch (node.status) {
+      case 'added':
+        return buildEntrieStr(stringify(value, valueDepth), '+');
+      case 'removed':
+        return buildEntrieStr(stringify(value, valueDepth), '-');
+      case 'nested':
+        return buildEntrieStr(buildObjectStr(iter(node.children, valueDepth), valueDepth));
+      case 'updated':
+        return [
+          buildEntrieStr(stringify(node.oldValue, valueDepth), '-'),
+          buildEntrieStr(stringify(node.newValue, valueDepth), '+'),
+        ].join('\n');
+      default:
+        return buildEntrieStr(stringify(value, valueDepth));
     }
-    case 'updated': {
-      return {
-        ...tree,
-        [`- ${node.key}`]: node.oldValue,
-        [`+ ${node.key}`]: node.newValue,
-      };
-    }
-    default:
-      return { ...tree, [node.key]: node.value };
-  }
-}, {});
+  });
 
-export default (data, replacer = ' ', spacesCount = 2) => {
-  const diffObject = buildDiffObject(data);
-  const iter = (currValue, depth) => {
-    if (!_.isObject(currValue)) return `${currValue}`;
-    const indentCount = depth * spacesCount + 2 * (depth - 1);
-    const currentIndent = replacer.repeat(indentCount);
-    const bracketIndent = replacer.repeat(indentCount - spacesCount);
-    const normalizeKey = (key) => (
-      key.startsWith('+') || key.startsWith('-') ? key : key.padStart(key.length + 2)
-    );
-    const strLines = Object.entries(currValue)
-      .map(([key, val]) => `${currentIndent}${normalizeKey(key)}: ${iter(val, depth + 1)}`);
-
-    return [
-      '{',
-      ...strLines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(diffObject, 1);
+  return buildObjectStr(iter(data));
 };
